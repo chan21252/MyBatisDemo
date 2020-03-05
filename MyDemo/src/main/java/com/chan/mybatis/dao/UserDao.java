a49cb0ae710f9e5d130e7aa872acec2109a96a27
chan <chanjobs21252@gmail.com>
2020-03-05 14:27:11 +0800

    Mapper映射文件

--- MyDemo/src/main/java/com/chan/mybatis/dao/UserDao.java
+++ MyDemo/src/main/java/com/chan/mybatis/dao/UserDao.java
@@ -2,6 +2,19 @@ package com.chan.mybatis.dao;
 
 import com.chan.mybatis.pojo.User;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserDao {
     public User selectUserById(int id);
+
+    public void insertUser(User user);
+
+    public void insertUserByMap(Map<String, Object> userMap);
+
+    public void updatePasswordById(User user);
+
+    public void deleteUserById(int id);
+
+    public void insertUserList(List<User> userList);
 }
--- MyDemo/src/test/java/MybatisTest.java
+++ MyDemo/src/test/java/MybatisTest.java
@@ -4,24 +4,27 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MybatisTest {
 
-    @Test
-    public void testDataSource() {
+    private final static String CONFIG_FILENAME = "mybatis.xml";
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
         InputStream inputstream = null;
         try {
-            inputstream = Resources.getResourceAsStream("mybatis.xml");
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputstream);
-            SqlSession session = sqlSessionFactory.openSession();
-            UserDao userDao = session.getMapper(UserDao.class);
-            User user = userDao.selectUserById(1);
-            System.out.println(user);
-            session.close();
+            inputstream = Resources.getResourceAsStream(CONFIG_FILENAME);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputstream);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -30,6 +33,93 @@ public class MybatisTest {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    @Test
+    public void testSelectUserById() {
+        SqlSession session = sqlSessionFactory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        User user = userDao.selectUserById(1);
+        Assert.assertEquals(1, user.getId());
+    }
+
+    @Test
+    public void testInsertUser() {
+        SqlSession session = sqlSessionFactory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        User user = new User();
+        user.setUsername("chan");
+        user.setPassword("abcdef");
+        userDao.insertUser(user);
+        session.commit();
+    }
+
+
+    @Test
+    public void testInsertUserByMap() {
+        SqlSession session = sqlSessionFactory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        Map<String, Object> userMap = new HashMap<String, Object>();
+        userMap.put("username", "chan");
+        userMap.put("password", "qwerty");
+
+        try {
+            userDao.insertUserByMap(userMap);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+    @Test
+    public void testUpdatePasswordById() {
+        SqlSession session = sqlSessionFactory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        User user = new User();
+        user.setId(3);
+        user.setPassword("123456");
+        try {
+            userDao.updatePasswordById(user);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+    @Test
+    public void testDeleteUserById() {
+        SqlSession session = sqlSessionFactory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        try {
+            userDao.deleteUserById(3);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+    @Test
+    public void testInsertUserList() {
+        SqlSession session = sqlSessionFactory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        List<User> userList = new ArrayList<User>();
+        for (int i = 0; i < 3; i++) {
+            User user = new User();
+            user.setUsername("test" + i);
+            user.setPassword("qwerty");
+            userList.add(user);
+        }
+
+        try {
+            userDao.insertUserList(userList);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
         }
     }
 }
